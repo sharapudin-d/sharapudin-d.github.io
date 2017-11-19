281033c01393f70949c7ce105c5e1c312ff20fb2
sharapudin-d <32812558+sharapudin-d@users.noreply.github.com>
2017-11-19 12:16:51 +0300

    m,

--- file1.js
+++ file1.js
@@ -1,1 +1,14 @@
-alert("hi, world");
+function kek()
+{
+  var a = document.getElementById("2").getAttribute("value");
+  if (a == "Показать")
+  {
+   document.getElementById("2").setAttribute("value", "Скрыть")
+   document.getElementById("1").setAttribute("type", "text")
+  }
+  else
+  {
+   document.getElementById("2").setAttribute("value", "Показать")
+   document.getElementById("1").setAttribute("type", "password")
+  }
+}
