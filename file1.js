b37f641366a5542256ab344d714b4d7ad68c344e
sharapudin-d <32812558+sharapudin-d@users.noreply.github.com>
2017-12-03 13:48:47 +0300

    v.3

--- file1.js
+++ file1.js
@@ -3,12 +3,34 @@ function kek()
   var a = document.getElementById("2").getAttribute("value");
   if (a == "Показать")
   {
-   document.getElementById("2").setAttribute("value", "Скрыть")
-   document.getElementById("1").setAttribute("type", "text")
+   document.getElementById("2").setAttribute("value", "Скрыть");
+   document.getElementById("1").setAttribute("type", "text");
   }
   else
   {
-   document.getElementById("2").setAttribute("value", "Показать")
-   document.getElementById("1").setAttribute("type", "password")
+   document.getElementById("2").setAttribute("value", "Показать");
+   document.getElementById("1").setAttribute("type", "password");
   }
+}
+function cheburek()
+{
+  var b = document.getElementById("1").getAttribute("value");
+  if (b == "ilbeback")
+  {
+    document.location.href = "demo/index.html";
+  }
+
+}
+function shrek()
+{
+    var a = prompt("Введите a");
+    var b = prompt("Введите b");
+    alert
+    (
+      a + b;
+      a - b;
+
+    );
+
+
 }
