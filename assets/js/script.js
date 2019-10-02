666e0dd7172ed47f42df70118abdb1241dd54db6
sharapudin-d <32812558+sharapudin-d@users.noreply.github.com>
2019-10-02 23:13:41 +0300

    Hotel Voyage v. 1.1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ $(function() {
  $(window).scroll(function() {
  var scr = $(this).scrollTop();
  $(".bg_img").css({
- "transform" : "translate(0%, " + scr/20 + "%)"
+ "transform" : "translate(0%, -" + scr/60 + "%)"
  });
  });
 });
